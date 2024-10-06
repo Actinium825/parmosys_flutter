1c1df526fdac166eba5b510beaf2ec15da296f2c
Actinium825 <cruz.d.ace@proton.me>
2024-10-06 19:33:39 +0800

    feat: screenshots

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:device_preview/device_preview.dart';
 import 'package:device_preview_screenshot/device_preview_screenshot.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
