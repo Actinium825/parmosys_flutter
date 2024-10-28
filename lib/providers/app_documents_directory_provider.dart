185ff73464ab2124746b45ddaab5821ab064356a
Actinium825 <cruz.d.ace@proton.me>
2024-10-29 00:19:51 +0800

    chore: update packages
    - update riverpod packages
    - add json annotation package
    - remove unnecessary models directory in exclude of analysis options
    - rename all ref parameters

--- lib/providers/app_documents_directory_provider.dart
+++ lib/providers/app_documents_directory_provider.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'app_documents_directory_provider.g.dart';
 
 @riverpod
-Directory appDocumentsDirectory(AppDocumentsDirectoryRef ref) => throw UnimplementedError();
+Directory appDocumentsDirectory(Ref ref) => throw UnimplementedError();
--- lib/providers/appwrite_client_provider.dart
+++ lib/providers/appwrite_client_provider.dart
@@ -1,10 +1,11 @@
 import 'package:appwrite/appwrite.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:parmosys_flutter/utils/env.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'appwrite_client_provider.g.dart';
 
 @riverpod
-Client appwriteClient(AppwriteClientRef ref) => Client()
+Client appwriteClient(Ref ref) => Client()
   ..setEndpoint(Env.endpoint)
   ..setProject(Env.projectId);
